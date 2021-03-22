20bea126a8a03c82208267685b716b55988bd9df
ChamoProgrammer <eldercol008@gmail.com>
2021-03-22 16:54:57 -0600

    online

---
+++ newslettter.php
@@ -0,0 +1,36 @@
+
+        <div id="newsletter" class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<div class="col-md-12">
+					
+						<div class="newsletter">
+							<p>Regístrese para<strong> Actualizaciones de ofertas</strong></p>
+							<form id="offer_form" onsubmit="return false">
+								<input class="input" type="email" id="email" name="email" placeholder="ingrese su correo porFavor...😉">
+								<button class="newsletter-btn" value="Sign Up" name="signup_button" type="submit"><i class="fa fa-envelope"></i> Suscribete</button>
+							</form>
+							<div class="" id="offer_msg">
+                                <!--Alert from signup form-->
+                            </div>
+							<ul class="newsletter-follow">
+								<li>
+									<a href="https://www.facebook.com/elder.col.1"><i class="fa fa-facebook"></i></a>
+								</li>
+								<li>
+									<a href="#"><i class="fa fa-twitter"></i></a>
+								</li>
+								<li>
+									<a href="#"><i class="fa fa-instagram"></i></a>
+								</li>
+								
+							</ul>
+						</div>
+					</div>
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
---
+++ register_form.php
@@ -0,0 +1,79 @@
+    <div class="wait overlay">
+        <div class="loader"></div>
+    </div>
+    <style>
+    .input-borders{
+        border-radius:30px;
+    }
+    </style>
+				<!-- row -->
+				
+                <div class="container-fluid">
+
+						<!-- /Billing Details -->
+						
+								<form id="signup_form" onsubmit="return false" class="login100-form">
+									<div class="billing-details jumbotron">
+                                    <div class="section-title">
+                                        <h2 class="login100-form-title p-b-49" >Registrarse Aqui...🤗</h2>
+                                    </div>
+                                    <div class="form-group">
+                                    
+                                        <input class="input form-control input-borders" type="text" name="f_name" id="f_name" placeholder="coloque su nombre por favor...😁">
+                                    </div>
+                                    <div class="form-group">
+                                    
+                                        <input class="input form-control input-borders" type="text" name="l_name" id="l_name" placeholder="coloque su apellido porfavor...😁">
+                                    </div>
+                                    <div class="form-group">
+                                        <input class="input form-control input-borders" type="email" name="email"  placeholder="su correo por favor...🙏">
+                                    </div>
+                                    <div class="form-group">
+                                        <input class="input form-control input-borders" type="password" name="password" id="password" placeholder="contraseña...🔑🔑">
+                                    </div>
+                                    <div class="form-group">
+                                        <input class="input form-control input-borders" type="password" name="repassword" id="repassword" placeholder="confirme su contraseña porfavor...🔑🙏">
+                                    </div>
+                                    <div class="form-group">
+                                        <input class="input form-control input-borders" type="text" name="mobile" id="mobile" placeholder="numero de telefono por favor...📲📲">
+                                    </div>
+                                    <div class="form-group">
+                                        <input class="input form-control input-borders" type="text" name="address1" id="address1" placeholder="anote su direccion porFavor...🙂🙏😁">
+                                    </div>
+                                    <div class="form-group">
+                                        <input class="input form-control input-borders" type="text" name="address2" id="address2" placeholder="coloque su ciudad...😉">
+                                    </div>
+                                    
+                                    
+                                    <div class="form-group">
+                                       <button class="primary-btn btn-block"  value="Sign Up" type="submit" name="signup_button"> Suscribirse</button>
+                                    </div>
+                                    <div class="text-pad">
+                                    <a href="" data-toggle="modal" data-target="#Modal_login">Ya tiene una Cuenta🤔?a continuación, iniciar sesión...😉🤙</a>
+                                       
+                                    </div>
+                                    
+                                
+								</form>
+                                <div class="login-marg">
+						<!-- Billing Details -->
+						<div class="row">
+                            <div class="col-md-2"></div>
+                            <div class="col-md-8" id="signup_msg">
+                                    
+
+                                </div>
+                                <!--Alert from signup form-->
+                            </div>
+                            <div class="col-md-2"></div>
+                        </div>
+
+						
+					</div>
+                    </div> 
+
+					
+				
+				<!-- /row -->
+
+			
